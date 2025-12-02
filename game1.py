4693b65f8fdd630fa43c768c1fa18d93dc13599a
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-02 20:38:40 +0100

    ajout ATH ( sans les sprites )

--- game1.py
+++ game1.py
@@ -18,11 +18,17 @@ class Game:
         self.player_bullets = pygame.sprite.Group()
         self.enemy_group = pygame.sprite.Group()
         self.enemy_bullets = pygame.sprite.Group()
+        
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Arial", 30)
+        self.small_font = pygame.font.SysFont("Arial", 20)
+
 
         # Temporaire 
         self.ennemi1 = Enemy(self.screen, self.Player, 200, 200)
         self.map.group.add(self.ennemi1)
         self.enemy_group.add(self.ennemi1)
+
 
 
     def run(self):
@@ -39,6 +45,9 @@ class Game:
             all_bullets.add(self.enemy_bullets)
 
             self.map.update(all_bullets)
+
+            self.draw_hud()
+
             self.screen.update()
 
 
@@ -55,3 +64,26 @@ class Game:
                     new_bullet = self.Player.fire()
                     self.player_bullets.add(new_bullet)
                     self.map.group.add(new_bullet)
+
+
+    def draw_hud(self):
+        display = self.screen.get_display()
+        
+        # Affichage du score
+        score_text = self.font.render(f"Points: {self.Player.point}", True, (255, 255, 255))
+        display.blit(score_text, (20, 40))
+
+        # Affichage de la vie
+        MAX_HP = 100
+        current_hp = 100
+
+        hp_bar_width = 150
+        hp_bar_height = 20
+
+        pygame.draw.rect(display, (50, 50, 50), (20, 10, hp_bar_width, hp_bar_height))
+
+        current_width = int((current_hp / MAX_HP) * hp_bar_width)
+        pygame.draw.rect(display, (0, 255, 0), (20, 10, current_width, hp_bar_height))
+
+        hp_text = self.font.render(f"HP: {current_hp}/{MAX_HP}", True, (255, 255, 255))
+        display.blit(hp_text,(20 + hp_bar_width + 10, 10))
