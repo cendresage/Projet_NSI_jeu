9f718d52a5afb2265c06c92474874e60dd162c19
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-03 09:58:57 +0100

    map0 en dev

---
+++ assets/map/garden.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="garden" tilewidth="16" tileheight="16" tilecount="473" columns="11">
+ <image source="../tileset/garden.png" width="190" height="700"/>
+</tileset>
