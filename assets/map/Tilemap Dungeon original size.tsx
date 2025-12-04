48d3330413dd174b5e00e8047e205e1dba5cfdf4
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-04 15:48:06 +0100

    ajout de map1

---
+++ assets/map/Tilemap Dungeon original size.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilemap Dungeon original size" tilewidth="16" tileheight="16" tilecount="600" columns="30">
+ <image source="../tileset/Tilemap Dungeon original size.png" width="480" height="320"/>
+</tileset>
