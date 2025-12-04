d2566474651db2d431643b897e68d2340f65bff9
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-04 14:56:53 +0100

    map0 terminer

---
+++ assets/map/2Crate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="2Crate" tilewidth="16" tileheight="16" tilecount="72" columns="24">
+ <image source="../../../../Downloads/Ranitayas_Dungeon_Pack_DEMO/Crates/2Crate.png" width="384" height="48"/>
+</tileset>
