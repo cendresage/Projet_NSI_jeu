b0a3577293cb896669cf94c821251e2010310b10
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-17 21:36:14 +0100

    systeme de music bien implanter

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame
 import sys
+
 from game1 import Game
 from menu import Menu, GameOverMenu
+from music import Music
 
 pygame.init()
 
 if __name__ == "__main__":
+
+    music_manager = Music()
+
     current_state = "menu"
     
     while True:
         if current_state == "menu":
+            music_manager.play("menu")
             menu = Menu()
             action = menu.run()
             if action == "play":
@@ -19,6 +25,7 @@ if __name__ == "__main__":
                 break
 
         elif current_state == "game":
+            music_manager.play("game")
             game = Game()
             result = game.run()
             if result == "game_over":
@@ -27,6 +34,7 @@ if __name__ == "__main__":
                 break
 
         elif current_state == "game_over":
+            music_manager.play("game_over")z
             game_over_menu = GameOverMenu()
             action = game_over_menu.run()
             if action == "menu":
