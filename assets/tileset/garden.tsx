91a456faf08663d634f925a4450bb3e390692e5f
Mathéo Gautron <cendresage11843@gmail.com>
2025-12-26 12:38:40 +0100

    création de la map pour le boss

--- assets/tileset/garden.tsx
+++ assets/tileset/garden.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="garden" tilewidth="16" tileheight="16" tilecount="473" columns="11">
- <image source="../../../../../Downloads/garden.png" width="190" height="700"/>
+ <image source="garden.png" width="190" height="700"/>
 </tileset>
